be73fb5ab8591cf6ccf9dbb84a03ca0ac96f8ed4
Charles Wu <No1CharlesWu@163.com>
2017-11-07 19:13:58 +0800

    删除多余项

--- frame/config_default.py
+++ frame/config_default.py
@@ -4,75 +4,13 @@ configs = {
         'port': 27017,
         'user': 'root',
         'password': "'",
-        'database': 'crawler'
+        'database': 'btc_ticker'
     },
     'task': {
-        'okcoin.cn': {
-            'rest': {
-                'btc': {
-                    'ticker',
-                    'depth_0.01',
-                    'depth_0.1',
-                    'depth_1',
-                    'trades',
-                    'kline_1min',
-                    'kline_3min'
-                },
-                'ltc': {
-                    'ticker',
-                    'depth_0.01',
-                    'depth_0.1',
-                    'depth_1',
-                    'trades',
-                    'kline_1min',
-                    'kline_3min'
-                }
-            }
-        },
         'okcoin.com': {
             'rest': {
                 'btc': {
-                    'ticker',
-                    'depth_0.01',
-                    'depth_0.1',
-                    'depth_1',
-                    'trades',
-                    'kline_1min',
-                    'kline_3min'
-                },
-                'ltc': {
-                    'ticker',
-                    'depth_0.01',
-                    'depth_0.1',
-                    'depth_1',
-                    'trades',
-                    'kline_1min',
-                    'kline_3min'
-                }
-            }
-        },
-        'huobi.com': {
-            'rest': {
-                'btc_cny': {
-                    'ticker',
-                    'depth',
-                    'detail',
-                    'kline_1min',
-                    'kline_5min'
-                },
-                'ltc_cny': {
-                    'ticker',
-                    'depth',
-                    'detail',
-                    'kline_1min',
-                    'kline_5min'
-                },
-                'btc_usd': {
-                    'ticker',
-                    'depth',
-                    'detail',
-                    'kline_1min',
-                    'kline_5min'
+                    'ticker'
                 }
             }
         }
