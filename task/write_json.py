029785ed60b8f49b7558280db17c1f4094a5104f
No1ChalresWu <No1CharlesWu@163.com>
2017-11-12 21:10:11 +0800

    修改了json输出格式，变成[{'type':'ticker','timestamp':2222222},{...}]这样的

--- task/write_json.py
+++ task/write_json.py
@@ -4,7 +4,7 @@ import copy
 from frame import taskbase
 
 all_dict = dict()
-
+all_list = list()
 class Task(taskbase.TaskBase):
     def do(self):
         print('write_json')
@@ -13,8 +13,13 @@ class Task(taskbase.TaskBase):
 
         with open("/Users/charles/Sites/test.json","w") as f:
             write_dict = copy.deepcopy(all_dict)
+            for k,v in write_dict.items():
+                v['type'] = k
+                all_list.append(v)
             # dumps 将数据转换成字符串
-            json.dump(write_dict,f)
+            # json.dump(write_dict,f)
+            json.dump(all_list, f)
+            all_list.clear()
             print("加载入文件完成...")
 
 if __name__ == '__main__':
